36f84033b88f308a4b94ab549fddebdf8dc5419a
RegularSvensson <elias.svensson.1992@gmail.com>
2017-03-02 17:48:55 +0100

    Add solution to 1.2)a)

--- hw5/main.m
+++ hw5/main.m
@@ -61,3 +61,51 @@ gscatter(X(:,1),X(:,2),Y2,'rb','.+');
 
 % Save figure
 print('11cdfigure','-dpng')
+
+%% 1.2)a)
+% Clear environment
+clear all;
+close all;
+clc;
+
+% Load data
+load('medium_100_10k.mat')
+
+% Choose K for kmeans clustering
+K = 10;
+
+% Choose number of words
+number_of_words = 10;
+
+% Calculate distances
+[~,~,~,D] = kmeans(wordembeddings,K);
+
+% Create cell array for topWords
+top_words = cell(number_of_words,K);
+
+% Iterate over clusters in distances
+for cluster = 1:size(D,2)
+
+    % Calculate sorting indices
+    [~, sort_index] = sort(D(:,cluster),'ascend');
+
+    % Get indices in number_of_words
+    index = sort_index(1:number_of_words);
+
+    % Print cluster number
+    fprintf('Cluster %d \n', cluster);
+
+    % Iterate over number of words
+    for i = 1:number_of_words
+
+        % Display word with index
+        disp(vocab{index(i)});
+
+        % Insert word into topwords
+        top_words{cluster,i} = vocab{index(i)};
+    end
+
+    % Print whitespace
+    fprintf('\n');
+end
+
